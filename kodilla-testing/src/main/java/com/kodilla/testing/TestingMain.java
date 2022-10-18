aedd4eb98c67074cc881f0c394fbcfb615860bb2
Oskar R <o.rajzner@gmail.com>
2022-10-18 18:09:23 +0200

    6.2 testing calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,17 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(5,10);
+        int resultSubtract = calculator.subtract(23,9);
+        if (resultAdd == 15 && resultSubtract == 14){
+            System.out.println("testOK");
+        }else {
+            System.out.println("Error");
+        }
+
     }
 }
